beec592dc76719d0d44f1461ea368f896ac07b5c
Aaron Williams <aaronrw4@yahoo.com>
2019-12-18 20:23:23 -0800

    Created Home Page

--- dev-desk/src/App.js
+++ dev-desk/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {BrowserRouter} from 'react-router-dom';
+import HomePage from './Components/homePage'
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
     <div className="App">
+      <HomePage/>
     </div>
     </BrowserRouter>
   );
---
+++ dev-desk/src/Components/homePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function HomePage() {
+    return(
+        <div>
+            <h1>Developement Desk Home Page</h1>
+            <button>Student</button>
+            <button>Helper</button>
+        </div>
+    )
+}
+
+export default HomePage;
