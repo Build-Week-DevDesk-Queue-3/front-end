7bc52a4bb728ab3942d97c720594c19c172fe750
Aaron Williams <aaronrw4@yahoo.com>
2020-01-08 20:17:03 -0800

    Added description and css.

--- dev-desk/src/Components/helper.js
+++ dev-desk/src/Components/helper.js
@@ -5,7 +5,6 @@ import LogOutHeader from './logOutHeader';
 function Helper() {
     const [tickets, setTickets] = useState([]);
     const [userTickets, setUserTickets] = useState([]);
-    const [selectedTicket, setSelectedTicket] = useState({})
     const userID = localStorage.getItem('id');
     const username = localStorage.getItem('name')
 
@@ -22,38 +21,30 @@ function Helper() {
         getTickets()
     },[]);
 
- //   useEffect(() => {
- //       const getUserTickets = () => {
- //           axiosWithAuth()
- //               .get('/userTickets')
- //               .then(res => {
- //                   setUserTickets(res.data);
- //               })
- //               .catch(err => console.log(err))
- //       };
- //       getUserTickets()
- //   },[])
+    useEffect(() => {
+        const getUserTickets = () => {
+            axiosWithAuth()
+                .get(`/ticket/users-tickets/${userID}`)
+                .then(res => {
+                    setUserTickets(res.data)
+                    console.log(res);
+                })
+                .catch(err => console.log(err))
+        };
+        getUserTickets()
+    },[])
 
-    const assignTicket = e => {
-        e.preventDefault();
-
+    const assignTicket = e => { 
+        let user = Number(userID);
+        let ticket = Number(e.target.value) 
+        console.log(user)     
+        console.log(ticket);       
         axiosWithAuth()
-            .put(`helper-student-ticket`, {
-                ticketId: selectedTicket.id,
-                studentId: selectedTicket.studentId,
-                helpId: userID
+            .put(`/ticket/assign`, {
+                userId: user,
+                ticketId: ticket                
             })
-            .then(
-                window.location.replace('#')
-            )
-            .catch(err => console.log(err));
-        
-        axiosWithAuth()
-            .put(`#`, {
-                id: selectedTicket.id,
-                title: selectedTicket.title,
-                
-            })
+            .catch(err => console.log(err.message));
     };
     
     return(
@@ -62,31 +53,35 @@ function Helper() {
             <div>
                 <h1>Welcome {username} to the Helper page</h1>
             </div>
-            <div>
-                <h3 className='ticketListHelper'>List of Tickets</h3>
-                {tickets.map(ticket => (
-                    <div className='ticketListHelper' key={ticket.id}>
-                        <p>Title: {ticket.title}</p>
-                        <p>Category: {ticket.category}</p>
-                        <p>Description: {ticket.description}</p>
-                        <p>Attempted: {ticket.attempted}</p>
-                        {ticket.resolved === true ? <p>Resolved</p> : null }
-                        {ticket.openStatus === false && ticket.resolved === false ? <p>Ticket Assigned</p> : null}
-                        {ticket.resolved === false && ticket.openStatus === true ? <button onClick={assignTicket}>Assign Ticket</button> : null}
-                    </div>
-                ))}
-                
-            </div>
-            <div>
-                {userTickets.map(ticket => (
-                    <div>
-                        <p>{ticket.title}</p>
-                        <p>{ticket.description}</p>
-                        <p>{ticket.attempted}</p>
-                        <button >Ticket Resolved</button>
-                        <button >Unassign Ticket</button>
-                    </div>
-                ))}
+            <div className='helperTicketsContainer'>
+                <div className='helperTicketSubcontainer'>
+                    <h3 className='ticketListHelper'>List of Tickets</h3>
+                    {tickets.map(ticket => (
+                        <div className='ticketListHelper' key={ticket.id}>
+                            <p>Title: {ticket.title}</p>
+                            <p>Category: {ticket.category}</p>
+                            <p>Description: {ticket.description}</p>
+                            <p>Attempted: {ticket.attempted}</p>
+                            {ticket.resolved === true ? <p>Resolved</p> : null }
+                            {ticket.openStatus === false && ticket.resolved === false ? <p>Ticket Assigned</p> : null}
+                            {ticket.resolved === false && ticket.openStatus === true ? <button onClick={assignTicket} value={ticket.id}>Assign Ticket</button> : null}
+                        </div>
+                    ))}
+                    
+                </div>
+                <div className='helperTicketSubcontainer'>
+                    <h3 className='assignedListHelper'>Tickets Assigned to You</h3>
+                    {userTickets.map(ticket => (
+                        <div className='assignedListHelper'>
+                            <p>Title: {ticket.title}</p>
+                            <p>Category: {ticket.category}</p>
+                            <p>Description: {ticket.description}</p>
+                            <p>Attempted: {ticket.attempted}</p>
+                            <button >Ticket Resolved</button>
+                            <button >Unassign Ticket</button>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     )
