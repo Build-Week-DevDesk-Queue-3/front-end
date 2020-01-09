c81f1b27cddf289197deff4f372a673c04616ced
dreampoetlee <dreampoetlee@gmail.com>
2020-01-09 03:21:55 -0500

    Worked on Dev-Desk App - Added the following components: studentActions, type, footer, Header, Profile, createTicket - Added the following npm packages: redux, formik, and antd - Need to finish building out the Profile, Sidebar, ticketlist components - Need help with the GET API stuff

---
+++ dev-desk/src/Components/Actions/types.js
@@ -0,0 +1,14 @@
+export const LOADING_UI = 'LOADING_UI';
+export const LOGIN = 'LOGIN';
+export const REGISTER = 'REGISTER';
+export const LOGOUT = 'LOGOUT';
+
+export const CREATE_TICKET = 'CREATE_TICKET';
+export const VIEW_TICKET = 'VIEW_TICKET';
+export const CLOSE_TICKET = 'CLOSE_TICKET';
+export const DELETE_TICKET = 'DELETE_TICKET';
+
+export const GET_ALL_TICKETS = 'GET_ALL_TICKETS';
+export const EDIT_TICKET = 'EDIT_TICKET';
+export const SEARCH_QUERY_CHANGE = 'SEARCH_QUERY_CHANGE';
+export const TOGGLE_CREATE_TICKET = 'TOGGLE_CREATE_TICKET';
