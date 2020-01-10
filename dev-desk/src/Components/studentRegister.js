e998033cd0909e5d13915cceeb5cc24250fd83ee
Aaron Williams <aaronrw4@yahoo.com>
2020-01-09 19:43:45 -0800

    Created register student

--- dev-desk/src/Components/studentRegister.js
+++ dev-desk/src/Components/studentRegister.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import registerImg from '../../src/register.svg';
+import axios from 'axios'
 
 export class studentRegister extends React.Component {
-  constructor(props) {
-    super(props);
+  state = {
+    username: '',
+    password: '',
+    message:''
+  } 
+
+  handleChange = e => {
+    this.setState({[e.target.name]: e.target.value})    
   }
-  
+
+  register = e => {
+    e.preventDefault();
+
+    axios
+      .post('/user/register', {
+        username: this.username,
+        password: this.password
+      })
+      .then(res => {
+        this.setState(res.message);
+        setTimeout(() => {window.location='/studentLogin'}, 2000)
+      })
+  }
+
   render() {
     return (
       <div className="base-container" ref={this.props.containerRef}>
@@ -19,26 +40,34 @@ export class studentRegister extends React.Component {
         
         <div className="form">
           <div className="form-group">
-            <label htmlFor="username">Username</label>
-            <input type="text" name="username" placeholder="username"/>
+            <label htmlFor="username">Username </label>
+            <input 
+              type="text" 
+              name="username" 
+              placeholder="username"
+              value={this.state.username}
+              onChange={this.handleChange}
+            />
           </div>
-          
+                    
           <div className="form-group">
-            <label htmlFor="email">Email</label>
-            <input type="text" name="email" placeholder="email"/>
-          </div>
-          
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input type="text" name="password" placeholder="password"/>
+            <label htmlFor="password">Password </label>
+            <input 
+              type="password" 
+              name="password" 
+              placeholder="password"
+              value={this.state.password}
+              onChange={this.handleChange}
+            />
           </div>
         </div>
         
         <div className="footer">
-          <button type="button" className="btn">
+          <button type="button" className="btn" onClick={this.register}>
             Register
           </button>
         </div>
+        <p>{this.message}</p>
       </div>
     );
   }
