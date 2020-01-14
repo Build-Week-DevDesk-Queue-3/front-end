92db79ad92d24495f1ccee7230ec9eae155d5cd5
dreampoetlee <dreampoetlee@gmail.com>
2020-01-13 22:03:41 -0500

    Working on Dev-Desk - Currently going through the app to fix all of the errors that happened from using Redux - Currently trying to fix the SignUp component to get it to render correctly

--- dev-desk/src/Pages/SignUp.js
+++ dev-desk/src/Pages/SignUp.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import axios from 'axios';
-import {withRouter} from 'react-router';
-import {ContainerWrap, PageHeader, ImageContent, StyledImg, FormWrapper, FormGroup, FormLabel, FormInput} from '../Components/Styles';
-import {Radio, Button, Icon} from 'antd';
-import registerImg from '../../src/SVG/register.svg';
 
+import {withRouter} from 'react-router';
+import {
+  ContainerWrap, PageHeader, ImageContent, FormLabel,
+  StyledImg, FormWrapper, FormGroup, FormInput
+} from '../Components/Styles';
+
+import {Radio, Button, Icon} from 'antd';
+import registerImg from '../SVG/register.svg';
 
 export class SignUp extends React.Component {
   constructor(props) {
@@ -12,109 +16,15 @@ export class SignUp extends React.Component {
   }
   
   state = {
-    username: '',
-    password: '',
-    message:'',
-    email: ''
-  } 
-
+    username: " ",
+    password: " ",
+    message: " ",
+    email: " "
+  };
+  
   handleChange = e => {
-    this.setState({[e.target.name]: e.target.value})    
-  }
+    this.setState({[e.target.name]: e.target.value});
+  };
   
-  handleRegisterClick(register) {
-    this.props.history.push('/studentDashboard')
-  }
-
-  register = e => {
-    e.preventDefault();
-    
-    axios
-      .post('https://dev-desk-que-3-bw.herokuapp.com/api/user/register', {
-        username: this.state.username,
-        password: this.state.password
-      })
-      .then(res => {
-        this.setState(res.message);
-        setTimeout(() => {window.location='/studentLogin'}, 2000)
-      })
-      .catch(err => console.log(err))
-  }
   
-  render() {
-    return (
-      <ContainerWrap ref={this.props.containerRef}>
-        <PageHeader>Dev Desk SignUp</PageHeader>
-        
-        <ImageContent>
-          <StyledImg src={registerImg} alt="Please Register"/>
-        </ImageContent>
-        
-        <FormWrapper>
-          <FormGroup>
-            <FormLabel htmlFor="username">Username</FormLabel>
-            
-            <FormInput
-              type="text"
-              name="username"
-              placeholder="username"
-              value={this.state.username}
-              onChange={this.handleChange}
-            />
-          </FormGroup>
-          
-          <FormGroup>
-            <FormLabel>Email</FormLabel>
-            
-            <FormInput
-              type="text"
-              name="email"
-              password="email"
-              value={this.state.email}
-              onChange={this.handleChange}
-            />
-          </FormGroup>
-          
-          <FormGroup>
-            <FormLabel>Password</FormLabel>
-            
-            <FormInput
-              type="text"
-              name="password"
-              placeholder="password"
-              value={this.state.password}
-              onChange={this.handleChange}
-              prefix={
-                <Icon type="lock" style={{color: 'rgba(0,0,0,.25)'}}/>
-              }
-            />
-          </FormGroup>
-          
-          <FormGroup>
-            <Radio.Group
-              name="role"
-              defaultValue={1}
-              onChange={this.handleChange}
-            />
-            
-            <Radio value={1}>I am a Student</Radio>
-            <Radio value={2}>I am a Helper</Radio>
-          </FormGroup>
-          
-          <FormGroup>
-            <Button
-              type="primary"
-              htmlType="submit"
-              className="signup-form-button"
-              style={{bacckground: '#2F6627', border: 'none', margin: '0'}}
-            >
-              Register
-            </Button>
-          </FormGroup>
-        </FormWrapper>
-      </ContainerWrap>    
-    );
-  }
 }
-
-export default withRouter(SignUp)
